9d9918e1eb3c02e0de2ef771dfd1475d2872712c
Ahmet Şeker <ahmetseker1905@hotmail.com.tr>
2025-05-19 15:10:29 +0300

    upfade

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/egzersiz-rehberi/',
-  plugins: [react()],
+  base: '/egzersiz-rehberi/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/404.html',
+          dest: '.' // dist/ klasörünün köküne kopyalanır
+        }
+      ]
+    })
+  ]
 })
